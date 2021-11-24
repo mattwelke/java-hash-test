f308ef594ac246b383448534940ed38e66cb29ac
Matt Welke <mattwelke@gmail.com>
2021-11-24 21:35:53 +0000

    cleanup output

--- src/main/java/com/javavscodestarter/App.java
+++ src/main/java/com/javavscodestarter/App.java
@@ -13,7 +13,7 @@ public class App
         String rawValue = "some-customer@example-email-provider.com";
         String salt = "468d5ed7-3c2b-402c-b49b-92adea5d574d"; // https://www.uuidgenerator.net/
         String input = String.format("%s%s", rawValue, salt);
-        System.out.println(String.format("The hash of %s with salt %s is:\n  %s.", rawValue, salt, hash(input)));
+        System.out.println(String.format("The hash of %s with salt %s is:\n  %s", rawValue, salt, hash(input)));
     }
 
     public static String hash(String rawValue) {
